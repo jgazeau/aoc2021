1308cd51d081cb15b48fb61e311af6c904ed0014
Jordan GAZEAU <jordan.gazeau@gmail.com>
2021-12-20 09:24:10 +0100

    feat: add README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Advent Of Code 2021
+
+:information_source: Don't bash me, first three days has not been made in kotlin :no_mouth:
+
+## Solutions
+
+* [Day1](./bash/day1)
+* [Day2](./bash/day2)
+* [Day3](./bash/day3)
+* [Day4](./src/main/kotlin/day4)
+* [Day5](./src/main/kotlin/day5)
+* [Day6](./src/main/kotlin/day6)
--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -5,18 +5,19 @@ import utils.DayInputs
 fun main() {
     val inputs = DayInputs(6)
     println("# TEST #")
-    inputs.testInput.process()
+    inputs.testInput.process(80)
+    inputs.testInput.process(256)
     println("# PUZZLE #")
-    inputs.input.process()
+    inputs.input.process(80)
+    inputs.input.process(256)
 }
 
 data class Population(var fishes: MutableList<Long>)
 
-fun List<String>.process() {
-    val duration = 256
+fun List<String>.process(duration: Int) {
     val lifecycle = first().initPopulation()
     repeat(duration) { lifecycle.iterate() }
-    println("--> Result: ${lifecycle.fishes.sum()} fish")
+    println("--> Result: ${lifecycle.fishes.sum()} fishes after $duration days")
 }
 
 fun String.initPopulation(): Population = Population(MutableList(9) { index -> split(",").count { it.toInt() == index }.toLong() })
